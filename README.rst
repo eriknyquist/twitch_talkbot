c0b78a9d17e146922c1d1e44057655ce99a82b64
Erik <eknyquist@gmail.com>
2020-10-26 19:12:03 -0700

    Add README, setup.py

---
+++ README.rst
@@ -0,0 +1,6 @@
+Twitch Talkbot
+==============
+
+A twitch bot for streamers who can't / don't want to speak.
+
+I'll write more stuff here someday...
---
+++ setup.py
@@ -0,0 +1,26 @@
+import unittest
+import os
+from setuptools import setup
+from distutils.core import Command
+
+from twitch_talkbot import __version__
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+README = os.path.join(HERE, "README.rst")
+
+with open(README, 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='twitch_talkbot',
+    version=__version__,
+    description=('A twitch bot for streamers who can\'t / don\'t want to talk'),
+    long_description=long_description,
+    url='http://github.com/eriknyquist/twitch_talkbot',
+    author='Erik Nyquist',
+    author_email='eknyquist@gmail.com',
+    license='Apache 2.0',
+    packages=['twitch_talkbot'],
+    include_package_data=True,
+    zip_safe=False
+)
