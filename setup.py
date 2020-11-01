accbc119c19738bf889b46c81432add036270c12
Erik Nyquist <eknyquist@gmail.com>
2020-10-31 20:51:35 -0700

    Fix missing dependencies, bump version

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ README = os.path.join(HERE, "README.rst")
 with open(README, 'r') as f:
     long_description = f.read()
 
+with open('requirements.txt', 'r') as fh:
+    requirements = [x.strip() for x in fh.readlines()]
+
 setup(
     name='twitch_talkbot',
     version=__version__,
@@ -22,5 +25,6 @@ setup(
     license='Apache 2.0',
     packages=find_packages(),
     include_package_data=True,
+    install_requires=requirements,
     zip_safe=False
 )
